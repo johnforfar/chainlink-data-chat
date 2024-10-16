c0c725c3463fd236800e8db367eff17f7443bd41
zimbatm <zimbatm@zimbatm.com>
2024-10-16 12:20:17 +0200

    nix: switch flake to use pkgs.buildNpmPackage
    Work around the inBundle issue by using a different npm builder.
    
    This one requires updating the `npmDepsHash` files whenever the
    package-lock.json is updated.
    
    Credits: package largely inspired by https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/ne/nextjs-ollama-llm-ui/package.nix

--- flake.nix
+++ flake.nix
@@ -1,48 +1,43 @@
 {
   description = "SmartCon Openmesh Demo Flake";
 
-  # We import the latest commit of dream2nix main branch and instruct nix to
-  # re-use the nixpkgs revision referenced by dream2nix.
-  # This is what we test in CI with, but you can generally refer to any
-  # recent nixpkgs commit here.
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.follows = "dream2nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-  }: let
-    # A helper that helps us define the attributes below for
-    # all systems we care about.
-    eachSystem = nixpkgs.lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-  in {
-    packages = eachSystem (system: {
-      # For each system, we define our default package
-      # by passing in our desired nixpkgs revision plus
-      # any dream2nix modules needed by it.
-      default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./default.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      # A helper that helps us define the attributes below for
+      # all systems we care about.
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
           }
-        ];
-      };
-    });
-  };
+        );
+    in
+    {
+      packages = eachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.callPackage ./nix/package.nix { };
+        }
+      );
+
+      checks = eachSystem (
+        { pkgs, system, ... }:
+        {
+          package = self.packages.${system}.default;
+        }
+      );
+    };
 }
