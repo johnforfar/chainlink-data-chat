a4bde3a9cd484686168745a35747f37e3f506e0c
Plopmenz <plopmenz@gmail.com>
2024-10-21 11:39:16 +0200

    title update

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -7,7 +7,7 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Openmesh SmartCon Demo
+          Openmesh Chainlink Data Dashboard
         </h1>
 
         <p className="max-w-[700px] text-base text-muted-foreground md:text-lg">
